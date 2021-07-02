3787828fd310a33223775e7add80116f33fd4731
kim0914 <kosa0914@naver.com>
2021-07-02 20:03:29 +0900

    2609_GCD/LCM

---
+++ PART1/2609.py
@@ -0,0 +1,41 @@
+######################### O(n) = logn #####################################
+a,b = input().split()
+a = int(a)
+b = int(b)
+small_val = 0
+large_val = 0
+gcd = 0
+lcm = 0
+
+if(a>b):
+    large_val = a
+    small_val = b
+else:
+    large_val = b
+    small_val = a
+
+remainder = large_val % small_val
+if(remainder == 0):
+    gcd = small_val
+
+while(remainder != 0):
+    gcd = remainder
+    remainder = small_val % remainder
+
+lcm = int(a * b / gcd)
+print(gcd)
+print(lcm)
+
+################ O(n) = N ####################
+# if(a>b):
+#     min_val = b
+# else:
+#     min_val = a
+
+# for i in range(1,min_val+1):
+#     if(a%i == 0 and b%i == 0):
+#         gcd = i
+#         lcm = int(i * (a/i) * (b/i))
+# print(gcd)
+# print(lcm)
+
