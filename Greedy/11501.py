91806f67067f101a800c1d38221bb98d82f3012d
kosa0914 <kosa0914@naver.com>
2021-07-29 19:35:52 +0900

    11501_주식

--- Greedy/11501.py
+++ Greedy/11501.py
@@ -9,14 +9,12 @@ for loop in range(T):
     stock_data = [int(x) for x in sys.stdin.readline().split()]
     
 
-    while(len(stock_data) > 0):
-        max_val = max(stock_data)
-        max_ind = stock_data.index(max_val)
+    max_val = stock_data[-1]
 
-        for i in range(max_ind):
-            profit = profit + (max_val - stock_data[i])
-            
-        stock_data = stock_data[max_ind + 1:]
-        
+    for i in range(N-1,-1,-1):
+        if stock_data[i] > max_val:
+            max_val = stock_data[i]
+        else:
+            profit += (max_val - stock_data[i])
+
     print(profit)
-
