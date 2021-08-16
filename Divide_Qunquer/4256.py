166edd71d28aaedc7fa9ceb2ef1549a99966fb94
kosa0914 <kosa0914@naver.com>
2021-08-16 23:20:03 +0900

    4256_트리

--- Divide_Qunquer/4256.py
+++ Divide_Qunquer/4256.py
@@ -1,5 +1,6 @@
 import sys
 
+# 루트를 기준으로 중위(inorder)순회한 결과의 왼쪽은 왼쪽 서브트리, 오른쪽 서브트리
 def postOrder(preorder, inorder):
     if len(preorder) == 0:
         return
@@ -15,15 +16,15 @@ def postOrder(preorder, inorder):
 
     root_idx = inorder.index(preorder[0])
 
-    inorder_left = inorder[0:root_idx]
-    preorder_left = preorder[1:len(inorder_left) + 1]
-    postOrder(preorder_left, inorder_left)
+    inorder_left = inorder[0:root_idx] # inorder에서는 처음부터 루트 인덱스 까지가 왼쪽 서브트리
+    preorder_left = preorder[1:len(inorder_left) + 1] # preorder에서는 1부터 inorder_left의 갯수만큼이 왼쪽 서브트리
+    postOrder(preorder_left, inorder_left) # 재귀적으로 반복
 
-    inorder_right = inorder[root_idx + 1:]
-    preorder_right = preorder[len(inorder_left) + 1:]
-    postOrder(preorder_right, inorder_right)
+    inorder_right = inorder[root_idx + 1:] # inorder에서는 루트 다음부터 마지막 까지가 오른쪽 서브트리
+    preorder_right = preorder[len(inorder_left) + 1:] # preorder에서는 inorder_right의 갯수만큼 부터 마지막 까지가 오른쪽 서브트리
+    postOrder(preorder_right, inorder_right) # 반복
 
-    print(preorder[0], end = ' ')
+    print(preorder[0], end = ' ') # 후위(postorder) 방식에서 루트노드는 제일 마지막 => 루트노드 값 출력
     
 
 
