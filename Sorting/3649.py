cc0ba4db51368a888b6e4d5842f0f8ca5f00d1ed
kosa0914 <kosa0914@naver.com>
2021-08-21 19:50:28 +0900

    3649_로봇 프로젝트(여러개 입력으로 수정)

--- Sorting/3649.py
+++ Sorting/3649.py
@@ -1,39 +1,45 @@
-width = int(input())  # cm
-n = int(input())      # rego count
+import sys
+while True:
+    try:
+        width = int(sys.stdin.readline())  # cm
+        n = int(sys.stdin.readline())      # rego count
 
-length = []
-res = []
-sum = 10000000 * width
+        length = []
+        res = []
+        sum = 10000000 * width
 
-for i in range(n):
-    length.append(int(input()))
+        for i in range(n):
+            length.append(int(sys.stdin.readline()))
 
-sort_length = sorted(length)
-
-
-
-for data in sort_length:
-    if data > 0:
-        diff = sum - data
-        
-        if diff in sort_length:
-            diff_index = sort_length.index(diff)
-            data_index = sort_length.index(data)
-            value = abs(data - sort_length[diff_index])
-            
-            if data <= sort_length[diff_index] and diff_index != data_index: 
-                res.append((data, sort_length[diff_index], value)) # [(a,b,c), ......]
-
-            elif data <= sort_length[diff_index] and sort_length.count(sort_length[diff_index]) > 1:
-                res.append((data, sort_length[diff_index], value))
-
-            else:
-                continue
-
-res.sort(key=lambda x:x[2], reverse=True)
+        sort_length = sorted(length)
 
 
-if len(res) == 0:
-    print('danger')
-else:
-    print('yes ' + str(res[0][0]) + ' ' + str(res[0][1]))
+
+        for data in sort_length:
+            if data > 0:
+                diff = sum - data
+                
+                if diff in sort_length:
+                    diff_index = sort_length.index(diff)
+                    data_index = sort_length.index(data)
+                    value = abs(data - sort_length[diff_index])
+                    
+                    if data <= sort_length[diff_index] and diff_index != data_index: 
+                        res.append((data, sort_length[diff_index], value)) # [(a,b,c), ......]
+
+                    elif data <= sort_length[diff_index] and sort_length.count(sort_length[diff_index]) > 1:
+                        res.append((data, sort_length[diff_index], value))
+
+                    else:
+                        continue
+
+        res.sort(key=lambda x:x[2], reverse=True)
+
+
+        if len(res) == 0:
+            print('danger')
+        else:
+            print('yes ' + str(res[0][0]) + ' ' + str(res[0][1]))
+    
+    except:
+        break
