ac17f4661b9905652a2d35432ffc70a90fbbc69c
kosa0914 <kosa0914@naver.com>
2021-08-23 23:10:20 +0900

    1260_DFS & BFS

--- BFS & DFS/1260.py
+++ BFS & DFS/1260.py
@@ -10,22 +10,22 @@ def dfs(v):
 
 def bfs(v):
     queue = [v]
-    visited[v] = 0
+    visited[v] = 0 # dfs를 먼저 수행했기 때문에 방문하였으면 0 으로 표시
     while queue:
         v = queue[0]
         print(v, end = ' ')
         del(queue[0])
         for i in range(1, n+1):
-            if visited[i] == 1 and mat[v][i] == 1:
+            if visited[i] == 1 and mat[v][i] == 1: # 방문하지 않은 vertex이고 v 정점과 연결되어 있으면
                 queue.append(i)
                 visited[i] = 0
                 
 
 n, m, v = map(int, input().split())
-mat = [[0] * (n + 1) for _ in range(n+1)]
+mat = [[0] * (n + 1) for _ in range(n+1)] # 정점 인덱스와 행렬 인덱스를 구분하기 쉽도록 0행, 0열은 0으로
 visited = [0 for _ in range(n+1)]
 
-for i in range(m):
+for _ in range(m):
     i, j = map(int, input().split())
     mat[i][j] = 1
     mat[j][i] = 1
