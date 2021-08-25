387e2463ab98f5153541e9da1e22d761e0dd0d7a
kosa0914 <kosa0914@naver.com>
2021-08-26 00:45:03 +0900

    1248_고양이 , 10171_알려줘

--- BackTracking/1248.py
+++ BackTracking/1248.py
@@ -2,13 +2,13 @@ def ck(idx):
     hap = 0
     for i in range(idx, -1, -1):
         hap += result[i]
-        if hap == 0 and S[i][idx] != 0:
+        if hap == 0 and S[i][idx] != 0: # 해당 값이 0인데 부호가 0이 아닐경우 False
             return False
-        elif hap < 0 and S[i][idx] >= 0:
+        elif hap < 0 and S[i][idx] >= 0: # 해당 값이 0 미만인데 부호가 -1이 아닐경우 False
             return False
-        elif hap > 0 and S[i][idx] <= 0:
+        elif hap > 0 and S[i][idx] <= 0: # 해당 값이  0보다 큰데 부호가 0보다 작거나 같은경우 False
             return False
-    return True
+    return True # 위의 조건문 다 걸리지 않는다면 True
 
 def solve(idx):
     if idx == N:
