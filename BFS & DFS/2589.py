3adaaacc2a8fe97d16d6ef42b1c9657ca1acc584
DongUk <68818952+Kim0914@users.noreply.github.com>
2021-08-29 21:01:07 +0900

    Delete 2589.py

--- BFS & DFS/2589.py
+++
@@ -1,9 +0,0 @@
-import sys
-m, n = map(int, sys.stdin.readline().split())
-
-treasure_map = [list(sys.stdin.readline().strip()) for _ in range(m)]
-
-land_map = [(i,j) for i in range(m) for j in range(n) if treasure_map[i][j] == 'L']
-
-for data in land_map:
-    print(data)
