7f4eee260bdddda899b3901c0c96d6b800d60834
kosa0914 <kosa0914@naver.com>
2022-02-14 23:30:51 +0900

    레벨2_n^2 배열 자르기

---
+++ 프로그래머스/LEVEL 2/n^2 배열 자르기.py
@@ -0,0 +1,12 @@
+def solution(n, left, right):
+    answer = []
+
+    for num in range(int(left), int(right)+1):
+        i, j = num // n, num % n
+        
+        cnt = max(i,j) + 1
+        answer.append(cnt)
+
+    return answer
+
+print(solution(3,0,8))
