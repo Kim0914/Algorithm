5db5ba9c9f05fe3c6aa03d3631842fb10b7cf813
kosa0914 <kosa0914@naver.com>
2022-02-19 22:41:44 +0900

    레벨2_프렌즈4브록(진행중....)

--- 프로그래머스/LEVEL 2/프렌즈4블록.py
+++ 프로그래머스/LEVEL 2/프렌즈4블록.py
@@ -1,11 +1,42 @@
+def check(i,j, board):
+    c = board[i][j]
+    
+    if  c == board[i][j+1] and c == board[i+1][j] and c == board[i+1][j+1]:
+        return True
+    else:
+        return False    
+
 def solution(m, n, board):
     answer = 0
-    block = []
-    for data in board:
-        block.append(data)
+    flag = True
+        
+    checked = [['X'] * n for _ in range(m)]
+    
+    # 전처리
+    for i in range(len(board)):
+        board[i] = list(board[i])
 
-    print(block)
+    # 같은 캐릭터 체크
+    for i in range(m-1):
+        for j in range(n-1):
+            if check(i,j,board):
+                checked[i][j] = 'O'
+                checked[i+1][j] = 'O' 
+                checked[i][j+1] = 'O' 
+                checked[i+1][j+1] = 'O'
+                  
+    # 같은 캐릭터 제거
+    tmp = []
+    for j in range(n):
+        c_str = ''
+        for i in range(m):
+            if checked[i][j] == 'X':
+                c_str += board[i][j]
+                
+        while len(c_str) < n:
+            c_str += 'X'
+        tmp.append(c_str)
+    
+    
+    print(tmp)
     return answer
-
-
-print(solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
