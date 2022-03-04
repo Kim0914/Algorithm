210539218d163f3f107bf153ac2b921897b35e15
kosa0914 <kosa0914@naver.com>
2022-03-04 21:11:51 +0900

    11279_최대 힙

--- Sorting/11279.py
+++ Sorting/11279.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 def insert(heap, num):
     heap.append(num)
     child_index = len(heap) - 1
@@ -24,26 +27,42 @@ def delete(heap):
     parent = 1
     child = 2
 
+    N = len(heap)
+
+    while child <= N - 1:
+        if child < N - 1 and heap[child] < heap[child + 1]:
+            child += 1
+        
+        if heap[child] <= tail:
+            break
+        
+        heap[parent] = heap[child]
+
+        parent = child
+        child = child * 2
+
+    if len(heap) != 1:
+        heap[parent] = tail
     
-    return True
+    return root
+    
 
 N = int(input())
-heap = []
+heap = [0]
 
 for _ in range(N):
     n = int(input())
         
     if n == 0:
-        if len(heap) > 0:
-            print(heap[1])
-            delete(heap)
+        if len(heap) > 1:
+            print(delete(heap))
         else:
             print(0)
     
     else:
         insert(heap, n)
-
-
-
-
     
+
+'''
+sys 라이브러리로 input을 처리하지 않으면 시간초과 발생함 !
+'''
